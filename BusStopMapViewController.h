01e446803e149d25f47579e6071d32deeacbc16e
Adam Lowther <adam@lamaservicesllc.com>
2013-04-13 01:01:24 -0400

    added bus view controller

---
+++ BusStopMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  BusStopMapViewController.h
+//  BusStop
+//
+//  Created by Adam Lowther on 4/13/13.
+//  Copyright (c) 2013 0xC0ffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BusStopMapViewController : UIViewController
+
+@end
---
+++ BusStopMapViewController.m
@@ -0,0 +1,38 @@
+//
+//  BusStopMapViewController.m
+//  BusStop
+//
+//  Created by Adam Lowther on 4/13/13.
+//  Copyright (c) 2013 0xC0ffee. All rights reserved.
+//
+
+#import "BusStopMapViewController.h"
+
+@interface BusStopMapViewController ()
+
+@end
+
+@implementation BusStopMapViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
