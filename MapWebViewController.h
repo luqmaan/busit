ed3ad5cf208c3b2a400ad9c536770365977f4d1d
Robert Ries <robertries@gmail.com>
2013-04-14 15:10:37 -0400

    adding pdf map

---
+++ MapWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  MapWebViewController.h
+//  BusStop
+//
+//  Created by Robert Ries on 4/14/13.
+//  Copyright (c) 2013 0xC0ffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapWebViewController : UIViewController
+
+@property (nonatomic, retain) IBOutlet UIWebView *webView;
+
+@end
