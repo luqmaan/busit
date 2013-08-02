1a82b581f9b9664f22d9731e745fae7ad1214153
Luqmaan <ldawoodjee@gmail.com>
2013-08-02 19:47:25 -0400

    Got rid of old pixate, added new better license. Got rid of other stuff from story board.

--- BusStop/main.m
+++ BusStop/main.m
@@ -15,6 +15,7 @@
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+        [PXEngine licenseKey:@"B4Q0F-A3ECP-C90JO-LVNS4-7304Q-U4C1R-M4BFI-IOILJ-39SQ4-OU79H-ND3LU-4ACRG-0DMNR-9STA5-URV5G-JU" forUser:@"ldawoodjee@gmail.com"];
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([BusStopAppDelegate class]));
     }
 }
