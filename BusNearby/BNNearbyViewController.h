eac483eedef144fac0dc3a628c10411548d3bbc0
Luqmaan <ldawoodjee@gmail.com>
2013-08-04 05:33:24 -0400

    Adding NearbyVC

---
+++ BusNearby/BNNearbyViewController.h
@@ -0,0 +1,13 @@
+//
+//  BNNearbyViewController.h
+//  BusStop
+//
+//  Created by Lolcat on 8/3/13.
+//  Copyright (c) 2013 0xC0ffee. All rights reserved.
+//
+
+#import "BusStopUIViewController.h"
+
+@interface BNNearbyViewController : BusStopUIViewController
+
+@end
---
+++ BusNearby/BNNearbyViewController.m
@@ -0,0 +1,38 @@
+//
+//  BNNearbyViewController.m
+//  BusStop
+//
+//  Created by Lolcat on 8/3/13.
+//  Copyright (c) 2013 0xC0ffee. All rights reserved.
+//
+
+#import "BNNearbyViewController.h"
+
+@interface BNNearbyViewController ()
+
+@end
+
+@implementation BNNearbyViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
