4e8113b810bd0b8430597f703b00c2a481dc1ad7
Luqmaan <ldawoodjee@gmail.com>
2013-08-12 22:54:35 -0400

    Improved styling on annotations

--- BMVehicleAnnotationView.m
+++ BMVehicleAnnotationView.m
@@ -71,26 +71,16 @@
     CGContextSetInterpolationQuality( context , kCGInterpolationHigh );
     
     // draw circle
-    CGPathRef roundedRectPath = [self newPathForRoundedRect:rect radius:self->pinSize/2.0f];
-    [bgColor set];
-    CGContextSetShadow(context, CGSizeMake(0.0f, 0.0f), 2.0f);
+    CGPathRef roundedRectPath = [self newPathForSquare:rect radius:self->pinSize/2.0f];
+//    [bgColor set];
     CGContextAddPath(context, roundedRectPath);
 	CGContextFillPath(context);
-    
-    // draw border around circle
-    CGContextSetStrokeColorWithColor(context, borderColor.CGColor);
-    CGContextSetLineWidth(context, 1);
-    CGContextSetLineJoin(context, kCGLineJoinRound);
-    CGContextBeginPath(context);
-    CGContextAddPath(context, roundedRectPath);
-    CGContextStrokePath(context);
-    
-    // dispose of cgpathref
     CGPathRelease(roundedRectPath);
+    [self drawGradient:(CGContextRef)context inRect:rect];
     
     // draw route number
     [self drawString:vehicle.routeShortName
-            withFont:[UIFont fontWithName:@"HelveticaNeue-CondensedBlack" size:self->fontSize]
+            withFont:[UIFont fontWithName:@"Avenir-Black" size:self->fontSize]
               inRect:rect];
 }
 
@@ -101,7 +91,7 @@
     // http://stackoverflow.com/questions/1302824/iphone-how-to-draw-text-in-the-middle-of-a-rect
     [textColor set];
     
-    CGRect textRect = CGRectMake(0, 2, contextRect.size.width, contextRect.size.height);
+    CGRect textRect = CGRectMake(0, 3, contextRect.size.width, contextRect.size.height);
     
     [string drawInRect:textRect
              withFont:font
@@ -109,10 +99,65 @@
             alignment:UITextAlignmentCenter];
 }
 
+- (void) drawGradient:(CGContextRef)context inRect:(CGRect)rect
+{
+    CFArrayRef colors = (__bridge CFArrayRef)[NSArray arrayWithObjects:(id)bgColor.CGColor, (id)borderColor.CGColor, nil];
+
+    CGGradientRef gradient = CGGradientCreateWithColors(NULL, colors, NULL);
+        
+    CGContextSaveGState(context);
+    CGContextAddEllipseInRect(context, rect);
+    CGContextClip(context);
+    
+    CGPoint startPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMinY(rect));
+    CGPoint endPoint = CGPointMake(CGRectGetMidX(rect), CGRectGetMaxY(rect));
+    CGContextDrawLinearGradient(context, gradient, startPoint, endPoint, 0);
+    CGGradientRelease(gradient), gradient = NULL;
+    CGContextRestoreGState(context);
+    
+    CGContextDrawPath(context, kCGPathStroke);
+}
+
+
 - (CGPathRef) newPathForRoundedRect:(CGRect)rect radius:(CGFloat)radius
 {
     // http://www.cocoanetics.com/2010/02/drawing-rounded-rectangles/
 
+    CGMutablePathRef retPath = CGPathCreateMutable();
+    
+	CGRect innerRect = CGRectInset(rect, radius, radius);
+    
+	CGFloat inside_right = innerRect.origin.x + innerRect.size.width;
+	CGFloat outside_right = rect.origin.x + rect.size.width - 1;
+	CGFloat inside_bottom = innerRect.origin.y + innerRect.size.height;
+	CGFloat outside_bottom = rect.origin.y + rect.size.height - 1;
+    
+	CGFloat inside_top = innerRect.origin.y;
+	CGFloat outside_top = rect.origin.y + 1;
+	CGFloat outside_left = rect.origin.x + 1;
+    
+	CGPathMoveToPoint(retPath, NULL, innerRect.origin.x, outside_top);
+    
+	CGPathAddLineToPoint(retPath, NULL, inside_right, outside_top);
+	CGPathAddArcToPoint(retPath, NULL, outside_right, outside_top, outside_right, inside_top, radius);
+	CGPathAddLineToPoint(retPath, NULL, outside_right, inside_bottom);
+	CGPathAddArcToPoint(retPath, NULL,  outside_right, outside_bottom, inside_right, outside_bottom, radius);
+    
+	CGPathAddLineToPoint(retPath, NULL, innerRect.origin.x, outside_bottom);
+	CGPathAddArcToPoint(retPath, NULL,  outside_left, outside_bottom, outside_left, inside_bottom, radius);
+	CGPathAddLineToPoint(retPath, NULL, outside_left, inside_top);
+	CGPathAddArcToPoint(retPath, NULL,  outside_left, outside_top, innerRect.origin.x, outside_top, radius);
+    
+	CGPathCloseSubpath(retPath);
+    
+	return retPath;
+}
+
+
+- (CGPathRef) newPathForSquare:(CGRect)rect radius:(CGFloat)radius
+{
+    // http://www.cocoanetics.com/2010/02/drawing-rounded-rectangles/
+    
     CGMutablePathRef retPath = CGPathCreateMutable();
     
 	CGRect innerRect = CGRectInset(rect, radius, radius);
